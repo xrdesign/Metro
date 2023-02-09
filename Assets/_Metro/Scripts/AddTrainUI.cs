d88ab260203a0b500d7a1eff649239146665a2f4
Yuzi <60211694+UUUZ0428@users.noreply.github.com>
2023-02-08 23:41:55 -0800

    added ghostTrain for visualization, and fixed bugs in addTrainUI

--- Assets/_Metro/Scripts/AddTrainUI.cs
+++ Assets/_Metro/Scripts/AddTrainUI.cs
@@ -6,17 +6,24 @@ using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Physics;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 
-
-public class AddTrainUI : MonoBehaviour, IMixedRealityPointerHandler
+public class AddTrainUI : MonoBehaviour, 
+    IMixedRealityPointerHandler,
+    IMixedRealitySourceStateHandler,
+    IMixedRealitySourcePoseHandler
 {
     private TMP_Text trainCountTxt;
-    public bool hasGenerated;
     public bool hasAdded;
 
-    public GameObject train;
+    //[SerializeField] public GameObject ghostTrain;
+    public GameObject ghostTrain;
     public int trackID;
     public int trackLineID;
+
+    public MixedRealityPose LeftPose;
+    public MixedRealityPose RightPose;
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +32,38 @@ public class AddTrainUI : MonoBehaviour, IMixedRealityPointerHandler
         trainCountTxt = this.transform.Find("Train_count").GetComponent<TMP_Text>();
         trainCountTxt.text = MetroManager.Instance.freeTrains.ToString();
         Debug.Log("freeTrains: " + MetroManager.Instance.freeTrains);
+        
+        //generate a ghostTrain for visual representation      
+        ghostTrain = new GameObject();
+        ghostTrain.name = "GhostTrain";
+        var t = ghostTrain.AddComponent<GhostTrain>();
+        
+        ghostTrain.transform.position = LeftPose.Position;
+        this.ghostTrain.SetActive(false);
+        //trainComponent.Hide();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         trainCountTxt.text = MetroManager.Instance.freeTrains.ToString();
-        hasGenerated = true;
+        //update the ghostTrain's position to match that of left controller
+        ghostTrain.transform.position = MetroManager.Instance.LController.transform.position;
+        //ghostTrain.transform.position = LeftPose.Position;
+        
+
+        
+        //check if the ghostTrain should be placed and make it Hide/show      
+        if (ghostTrain!= null && MetroManager.Instance.addedTrain)
+        {
+            this.ghostTrain.SetActive(false);
+
+            //GhostTrain trainComponent = this.ghostTrain.GetComponent<GhostTrain>();
+        } else if (MetroManager.Instance.addingTrain)
+        {
+            this.ghostTrain.SetActive(true);
+        }
     }
 
     public void wasClicked()
@@ -39,39 +71,36 @@ public class AddTrainUI : MonoBehaviour, IMixedRealityPointerHandler
         Debug.Log("item clicked");
     }
 
-    public void AddTrain()
+    public bool CanPlaceTrain()
     {
+        return MetroManager.Instance.freeTrains > 0;
+    }
+    
+    public void AddTrain()
+    {   
         //get positin of the UI element
         var UIpos = GameObject.Find("TestButton").transform.position;
 
-        //check if there are trains available and prevent generating multiple trains at the same time
-        if (MetroManager.Instance.freeTrains > 0 && !hasGenerated)
+        //prevent adding multiple trains when the player only clicks UI once
+        MetroManager.Instance.addedTrain = false;
+
+        //creates visual reference of the added train
+        bool canPlaceTrain = CanPlaceTrain();
+        Debug.Log("canPlaceTrain: " + canPlaceTrain);
+        Debug.Log("freeTrains: " + MetroManager.Instance.freeTrains);
+
+        //check if there is available trains
+        if (MetroManager.Instance.freeTrains > 0)
         {
             MetroManager.Instance.addingTrain = true;
-            train = new GameObject();
-            train.name = "Train";
-            var t = train.AddComponent<Train>();
-            //get and set train's position next to the UI panel
-            var TrainPos = t.transform.position;
-            TrainPos = UIpos;
-            TrainPos.x = UIpos.x + 0.2f;
-            t.transform.position = TrainPos;
-
-            MetroManager.Instance.freeTrains--;
-            //trainCountTxt.text = MetroManager.Instance.freeTrains.ToString();
-            //hasGenerated = true;
         }
-        
+
     }
 
     void IMixedRealityPointerHandler.OnPointerDown(MixedRealityPointerEventData eventData)
     {
-        Debug.Log("UI clicked");
-        Debug.Log(eventData.Pointer.Position);
-        //prevent adding multiple trains when the player only clicks UI once
-        MetroManager.Instance.addingTrain = true;
-        MetroManager.Instance.addedTrain = false;
 
+        
     }
 
     void IMixedRealityPointerHandler.OnPointerUp(MixedRealityPointerEventData eventData)
@@ -87,5 +116,45 @@ public class AddTrainUI : MonoBehaviour, IMixedRealityPointerHandler
     void IMixedRealityPointerHandler.OnPointerClicked(MixedRealityPointerEventData eventData)
     {
 
+    }
+
+    void IMixedRealitySourcePoseHandler.OnSourcePoseChanged(SourcePoseEventData<MixedRealityPose> eventData)
+    {
+        //Debug.Log(eventData.Controller.ControllerHandedness);
+        if (eventData.Controller.ControllerHandedness == Handedness.Left)
+        {
+            LeftPose = eventData.SourceData;
+            //Debug.Log("controller pos" + LeftPose.Position);
+        }
+        else if (eventData.Controller.ControllerHandedness == Handedness.Right)
+        {
+            RightPose = eventData.SourceData;
+        }
+        //Debug.Log(eventData.SourceData);
+    }
+    void IMixedRealitySourcePoseHandler.OnSourcePoseChanged(SourcePoseEventData<TrackingState> eventData)
+    {
+        // Debug.Log(eventData.SourceData);
+    }
+    void IMixedRealitySourcePoseHandler.OnSourcePoseChanged(SourcePoseEventData<Vector2> eventData)
+    {
+        // Debug.Log(eventData.SourceData);
+    }
+    void IMixedRealitySourcePoseHandler.OnSourcePoseChanged(SourcePoseEventData<Vector3> eventData)
+    {
+        Debug.Log(eventData.SourceData);
+    }
+    void IMixedRealitySourcePoseHandler.OnSourcePoseChanged(SourcePoseEventData<Quaternion> eventData)
+    {
+        // Debug.Log(eventData.SourceData);
+    }
+
+    void IMixedRealitySourceStateHandler.OnSourceDetected(SourceStateEventData eventData)
+    {
+        // Debug.Log(eventData);
+    }
+    void IMixedRealitySourceStateHandler.OnSourceLost(SourceStateEventData eventData)
+    {
+        // Debug.Log(eventData);
     }
 }
