0e8c715aac9be2d656f3c6a77d4c0f44ca06964c
Eshaan Sharma <eshaansharma18@gmail.com>
2025-03-25 21:11:49 -0600

    added TTS, input from python server

--- Assets/_Metro/Scoring/TestServer.py
+++ Assets/_Metro/Scoring/TestServer.py
@@ -24,18 +24,33 @@ def get_state_from_game(ws, game_id: int):
     state = send_and_recv(ws, {"command":"get_state_sync", "game_id": game_id})
     return state
 
+def send_response(ws, response):
+    message = {
+        "command": "speak",
+        "response" : response
+    }
+    ws.send(json.dumps(message))
+
+
 ws = websocket.create_connection(url)
 
-start_time = time.time()
-count = 0
+# start_time = time.time()
+# count = 0
+# while True:
+#     result = get_state_from_game(ws, 0)
+#     if result is None:
+#         break
+#     count += 1
+#     # print the rate of requests per second
+#     if count % 100 == 0:
+#         print(count / (time.time() - start_time))
+#     time.sleep(0.001)
+
+test_response = ""
 while True:
-    result = get_state_from_game(ws, 0)
-    if result is None:
-        break
-    count += 1
-    # print the rate of requests per second
-    if count % 100 == 0:
-        print(count / (time.time() - start_time))
-    time.sleep(0.001)
+    test_response = input("Input text:")
+    print(test_response)
+    send_response(ws, test_response)
+
 
 
--- Assets/_Metro/Scripts/DeepgramConnection.cs
+++ Assets/_Metro/Scripts/DeepgramConnection.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+// TTS IMPORTS
+using RestSharp;
+using System.IO;
+
+using UnityEngine.Networking;
 using NativeWebSocket;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+
 
 [System.Serializable]
 public class DeepgramResponse
@@ -28,6 +37,9 @@ public class Alternative
 
 public class DeepgramConnection : MonoBehaviour
 {
+    public AudioSource microphoneSource;
+    public AudioSource ttsSource;
+
     AudioSource _audioSource;
     int lastPos, curPos;
 
@@ -40,13 +52,26 @@ public class DeepgramConnection : MonoBehaviour
 
     private string command = "";
 
+    //TODO: debug
+    public string testString = "";
+    public bool test = false;
 
+    private byte[] ttsAudioBytes = null;
+    private object ttsLock = new object();
 
 
     void Awake()
     {
-        _audioSource = GetComponent<AudioSource>();
-        Debug.Assert(_audioSource != null, "DeepgramConnection missing Audio Source");
+        if (microphoneSource == null)
+        {
+            microphoneSource = gameObject.AddComponent<AudioSource>();
+        }
+        if (ttsSource == null)
+        {
+            ttsSource = gameObject.AddComponent<AudioSource>();
+        }
+        // _audioSource = GetComponent<AudioSource>();
+        // Debug.Assert(_audioSource != null, "DeepgramConnection missing Audio Source");
         MetroManager.Instance.deepgramConnection = this;
     }
     // Start is called before the first frame update
@@ -55,8 +80,9 @@ public class DeepgramConnection : MonoBehaviour
         //Connect to Mic
         Debug.Log("Connecting to mic");
         Debug.Assert(Microphone.devices.Length > 0, "No microphone available for DeepgramConnection");
-        _audioSource.clip = Microphone.Start(null, true, 30, AudioSettings.outputSampleRate);
-        _audioSource.Play();
+        microphoneSource.clip = Microphone.Start(null, true, 30, AudioSettings.outputSampleRate);
+        microphoneSource.loop = true;
+        microphoneSource.Play();
         curPos = 0; lastPos = 0;
         SetupWebsocket();
 
@@ -103,9 +129,9 @@ public class DeepgramConnection : MonoBehaviour
             }
             if (curPos - lastPos > 0)
             {
-                int numSamples = (curPos - lastPos) * _audioSource.clip.channels;
+                int numSamples = (curPos - lastPos) * microphoneSource.clip.channels;
                 float[] samples = new float[numSamples];
-                _audioSource.clip.GetData(samples, lastPos);
+                microphoneSource.clip.GetData(samples, lastPos);
 
                 //Convert to byte[] for deepgram
                 short[] samplesAsShorts = new short[numSamples];
@@ -207,6 +233,38 @@ public class DeepgramConnection : MonoBehaviour
         stop = true;
     }
 
+    public void Speak(string text) {
+
+        Debug.Log("[DeepgramConnection] Testing Speak for " + text);
+        var client = new RestClient("https://api.deepgram.com/v1/speak?model=aura-asteria-en");
+
+
+        var request = new RestRequest("/", Method.Post);
+        request.AddHeader("Authorization", $"Token {API_Key}");
+        request.AddHeader("Content-Type", "application/json");
+
+        request.AddParameter("application/json", $"{{\n  \"text\": \"{text}\"\n}}", ParameterType.RequestBody);
+        var response = client.Execute(request);
+
+        Debug.Log("[DeepgramConnection] Send request for " + text);
+
+        if (response.IsSuccessful && response.RawBytes != null) {
+            Debug.Log("[DeepgramConnection] Response successful for " + text);
+
+            lock (ttsLock)
+            {
+                ttsAudioBytes = response.RawBytes;
+            }
+           
+        }
+        else
+        {
+            Debug.Log("[DeepgramConnection] Response not successful for" + text);
+            Debug.LogError("Error in TTS request: " + response.StatusCode);
+            Debug.LogError(response.Content);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -234,5 +292,87 @@ public class DeepgramConnection : MonoBehaviour
             }
         }
 
+        if(test){
+            Speak(testString);
+            test = false;
+        }
+
+        byte[] audioBytes = null;
+        lock (ttsLock)
+        {
+            if (ttsAudioBytes != null) {
+                audioBytes = ttsAudioBytes;
+                ttsAudioBytes = null;
+            }
+        }
+        if (audioBytes != null) {
+            string filePath = Path.Combine(Application.persistentDataPath, "ttsAudio.mp3");
+            try
+            {
+                File.WriteAllBytes(filePath, audioBytes);
+                Debug.Log("Audio file saved as ttsAudio.mp3");
+                Debug.Log("Persistent Data Path: " + Application.persistentDataPath);
+                StartCoroutine(PlayAudio(filePath));
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Failed to save audio file: " + ex.Message);
+            }
+        }
+
     }
+
+    private IEnumerator PlayAudio(string filePath)
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("file://" + filePath, AudioType.MPEG))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Playing audio...");
+                AudioClip audioClip = DownloadHandlerAudioClip.GetContent(www);
+                ttsSource.clip = audioClip;
+                ttsSource.Play();
+            }
+            else
+            {
+                Debug.LogError("Failed to load audio file: " + www.error);
+            }
+        }
+    }
+    
 }
+
+
+// ---------MP3--------
+// curl \
+//   -X POST \
+//   -H "Authorization: Token 5b0227b54f76d599ca4feb05640d73b5a31f8aa7" \
+//   -H "Content-Type: application/json" \
+//   --data '
+//   {
+//     "text":"Hello, how can I help you today?"
+//   }
+//   ' \
+//   "https://api.deepgram.com/v1/speak?model=aura-asteria-en" \
+//   -o jsonAudio.mp3
+
+
+// ---------WAV--------
+// curl \
+//   -X POST \
+//   -H "Accept: audio/wav" \
+//   -H "Authorization: Token 5b0227b54f76d599ca4feb05640d73b5a31f8aa7" \
+//   -H "Content-Type: application/json" \
+//   --data '
+//   {
+//     "text":"Hello, how can I help you today?"
+//   }
+//   ' \
+//   "https://api.deepgram.com/v1/speak?model=aura-asteria-en" \
+//   -o jsonAudio.wav
+
+
+// ---------RUNNING PYTHON SERVER--------
+// Run Python Server --> /usr/bin/python3 "/Users/eshaansharma/Desktop/SCCN Research/DeepGram/Metro/Assets/_Metro/Scoring/TestServer.py"
