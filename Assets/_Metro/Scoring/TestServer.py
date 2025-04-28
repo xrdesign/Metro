67abafcc06ace1860bc00e690b5ed0d45954f447
Weichen Liu <WeichenLiu@users.noreply.github.com>
2025-04-27 23:52:56 -0700

    Update TestServer.py

--- Assets/_Metro/Scoring/TestServer.py
+++ Assets/_Metro/Scoring/TestServer.py
@@ -24,18 +24,42 @@ def get_state_from_game(ws, game_id: int):
     state = send_and_recv(ws, {"command":"get_state_sync", "game_id": game_id})
     return state
 
+def send_response(ws, response):
+    message = {
+        "command": "speak",
+        "response" : response
+    }
+    ws.send(json.dumps(message))
+
+
 ws = websocket.create_connection(url)
 
-start_time = time.time()
-count = 0
+
+# get state from game every 1 second
 while True:
     result = get_state_from_game(ws, 0)
     if result is None:
         break
-    count += 1
-    # print the rate of requests per second
-    if count % 100 == 0:
-        print(count / (time.time() - start_time))
-    time.sleep(0.001)
+    print(result)
+    # check if there is a new instruction and if so, get the instruction text
+    # var stateRes = new JSONObject();
+    # try
+    # {
+    #     stateRes = MetroManager.SerializeGame(gameIDGetStateSync);
+    #     stateRes.AddField("new_instructions", MetroManager.HasInstructions());
+    #     stateRes.AddField("instruction_text", MetroManager.GetInstructions());
 
+    #     Send(stateRes.ToString()); // Respond after Update() runs
+    # }
 
+    if result["new_instructions"]:
+        instruction_text = result["instruction_text"]
+        print(instruction_text)
+
+        if "hello" in instruction_text.lower() or "hi" in instruction_text.lower() or "good morning" in instruction_text.lower() or "good afternoon" in instruction_text.lower():
+            send_response(ws, "Hello, how can I help you?")
+        elif "who" in instruction_text.lower():
+            send_response(ws, "I am a virtual assistant")
+        else:
+            send_response(ws, "I am not sure how to help you")
+    time.sleep(1)
